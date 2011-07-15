913a466986b8bc48da3a95abdbfc35863071682a
Daniel Bretoi <nefar@otherware.org>
2011-07-14 23:38:36 -0700

    terminate if no cfg file

--- lib/rss-growl/config.rb
+++ lib/rss-growl/config.rb
@@ -2,10 +2,12 @@ class RSSGrowl
   class Config
     attr_accessor :settings
     def initialize(file=nil)
-      file = "#{ENV['HOME']}/.rss_growl"
-      missing_config unless File.exists? file
-      @settings = YAML.load(fh = open(file))
+      missing_config unless File.exists? cfg_file
+      @settings = YAML.load(fh = open(cfg_file))
       fh.close
+    end
+    def cfg_file
+      "#{ENV['HOME']}/.rss_growl"
     end
     def size
       settings.size
@@ -17,13 +19,14 @@ class RSSGrowl
     end
 
     def missing_config
-      puts %|create .rss_growl with example format:
+      puts %|create #{cfg_file} with example format:
         Woot:
           url: http://www.woot.com/Blog/Feed.ashx
           interval: 60
         Ron Paul:
           url: http://www.ronpaul2012.com/feed
       |
+      exit(1)
     end
   end
 end
